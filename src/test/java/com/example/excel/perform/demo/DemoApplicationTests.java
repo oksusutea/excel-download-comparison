8f6916dfcd475e8f364eee76752b398de766e13f
oksusutea <chanjukim118@gmail.com>
2021-07-21 19:19:04 +0900

    first commit

---
+++ src/test/java/com/example/excel/perform/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excel.perform.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
